0cff0c11a40c6354f929e3ab8880204277329b90
kshitij <kshitijjadhv3333@gmail.com>
2022-12-14 16:32:05 +0530

    add space

--- src/main/java/maven07/BaseNew.java
+++ src/main/java/maven07/BaseNew.java
@@ -21,6 +21,15 @@ public class BaseNew
 	Reporter.log("Launching browser",true);
 	Utility.wait(driver, 1000);
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public static void closingBrowser(WebDriver driver) throws 
 	InterruptedException
 	{
